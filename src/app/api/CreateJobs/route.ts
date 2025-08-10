b3c97b77f9d887184b94903d427a95e14d2ff64b
Riad <riadk910@gmail.com>
2025-08-10 21:24:23 +0600

    Updated name

---
+++ src/app/api/CreateJobs/route.ts
@@ -0,0 +1,31 @@
+import { NextResponse } from "next/server";
+import { auth } from "../../../../auth";
+import { NextRequest } from "next/server";
+import { prisma } from "@/lib/prisma";
+
+export async function POST(request: NextRequest) {
+  const session = await auth();
+
+  if (!session?.user || !session.user.id) {
+    return NextResponse.redirect(new URL("/auth/signin", request.url));
+  }
+
+  try {
+    const data = await request.json();
+
+    const job = await prisma.job.create({
+      data: {
+        ...data,
+        postedById: session.user.id,
+      },
+    });
+
+    return NextResponse.json(job);
+  } catch (error) {
+    console.error("Error creating job:", error);
+    return NextResponse.json(
+      { error: "Failed to create job" },
+      { status: 500 }
+    );
+  }
+}
